ab3289a4780de7e61822bd0dfb5eb95ee1fb039a
Charissa Smith <smith.charissa@gmail.com>
2011-04-16 16:17:52 -0400

    now it works - thanks JMaxx

--- spec/routing/application_spec.rb
+++ spec/routing/application_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe 'Application Routing'  do
-  describe 'ThoughtsController' do 
+  describe ThoughtsController do 
     it 'recognizes and generates thought_path' do
-    {:get => :thoughts_path}.should route_to :controller => 'thoughts', :action => 'index'
+    {:get => thoughts_path}.should route_to :controller => 'thoughts', :action => 'index'
   end
 end
 end
